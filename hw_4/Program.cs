8a4433c6fb2fdcca93b55cc70e9562b98bfd7597
Sagou <anastasiyazadybinina@gmailcom>
2022-08-06 10:48:15 +0300

    hw_4

---
+++ hw_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите любое целое число: ");
+
+string? N = Console.ReadLine();
+
+int number;
+
+if (int.TryParse(N, out number))
+{
+    int i = 0;
+    while (i < number-1)
+    {
+        i+=2;
+        Console.Write(i);
+        Console.Write(",");
+    }
+}
+else
+{
+    Console.Write("Ошибка ввода!");
+}
