bb4ff870c205612dfb4aa7ec3c96cc3fe84a41ef
Sagou <anastasiyazadybinina@gmailcom>
2022-08-11 15:04:37 +0300

    hw_6

---
+++ hw_6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+double number = new Random().Next(1, 1000);
+
+Console.WriteLine($"Случайное число = {number}");
+double x = number / 100;
+if (x > 1)
+{
+    double a = number % 10;
+    Console.WriteLine($"Третья цифра числа = {a}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
