e3c866171bb7b42567447768da1172b4a2d9026a
Sagou <anastasiyazadybinina@gmailcom>
2022-08-14 13:24:29 +0300

    hw_8

---
+++ hw_8/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Палиндро́м, пе́ревертень — число, буквосочетание, слово или текст, одинаково читающееся в обоих направлениях. 
+
+int Number = 0;
+InputNumber(ref Number);
+CheckNumber(Number);
+static void InputNumber(ref int number)
+{
+    try
+    {
+        Console.WriteLine("Введите пятизначное число: ");
+        number = int.Parse(Console.ReadLine() ?? "");
+        if (number < 10000)
+        {
+            Console.WriteLine("Это не пятизночное число, пожалуйста, введите пятизначное!");
+        }
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+static void CheckNumber(int number)
+{
+    int a1 = number / 10000;
+    int a5 = number % 10;
+    int a2 = (number % 10000) / 1000;
+    int a4 = (number % 100) / 10;
+
+    if (a1 == a5 && a2 == a4)
+    {   
+        Console.WriteLine($"Это палиндром: {number}");
+    }
+    else
+    {
+        Console.WriteLine($"Это не палиндром: {number}");
+    }
+}
+
+
