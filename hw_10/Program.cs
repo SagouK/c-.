991a14cc9b0e7127014275b29953caf090523d97
Sagou <anastasiyazadybinina@gmailcom>
2022-08-14 15:08:15 +0300

    hw_10

---
+++ hw_10/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int N = 0;
+
+InputNumber(ref N);
+PrintTableOfCubes(N);
+
+static void InputNumber(ref int n)
+{
+    try
+    {
+        Console.WriteLine("Введите любое число: ");
+        n = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Введены неверные данные! {exc.Message}");
+        return;
+    }
+}
+static void PrintTableOfCubes (int n)
+{
+    int index = 0;
+    double cube = 1;
+    while (index < n)
+    {
+        cube = Math.Pow(cube, 3);
+        Console.Write($"{cube},");
+        index++;
+        cube = index + 1;
+    }
+}
