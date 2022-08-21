0579851366e8372199114031cd7b753a97aa0c15
Sagou <anastasiyazadybinina@gmailcom>
2022-08-21 11:35:30 +0300

    hw_15

---
+++ hw_15/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = FillArray(6, -100, 100);
+Console.WriteLine(String.Join(" ", array));
+
+int uneven = sumUnevenNumbers(array);
+Console.WriteLine($"Сумма элементов массива, стоящих на нечетных позициях = {uneven}");
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+    return arr;
+}
+
+int sumUnevenNumbers(int[] finalArray)
+{
+    int sum = 0;
+    int length = finalArray.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (i % 2 > 0)
+        {
+            sum += finalArray[i];
+        }
+    }
+    return sum;
+}
