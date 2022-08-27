272436b972cb80fb33cb004fb580f3d216cb011b
Sagou <anastasiyazadybinina@gmailcom>
2022-08-27 15:17:43 +0300

    hw_19

---
+++ hw_19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+GetArray(3, 4);
+//Console.Write($"{array[rows, columns]}");
+
+void GetArray(int rows, int columns)
+{
+    double[,] arr = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i,j] = new Random().Next(-10, 10);
+            Console.Write($"{arr[i, j]} ");
+        } 
+    Console.WriteLine();
+    }   
+}
