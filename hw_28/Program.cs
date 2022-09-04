a5761c8082bfd8bc0a521a8ff7966ceefd4e8104
Sagou <anastasiyazadybinina@gmailcom>
2022-09-04 12:20:42 +0300

    hw_28

--- hw_28/Program.cs
+++ hw_28/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите n ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"Результат функции А({m},{n}) = {GetAckermannfunction(m,n)}");
+
+int GetAckermannfunction(int x, int y)
+{
+    if (x == 0) return y + 1;
+    else
+    if ((y == 0) && (n != 0)) return GetAckermannfunction(x-1,1);
+    else
+    return GetAckermannfunction(x-1, (GetAckermannfunction(x, y - 1)));
+}
