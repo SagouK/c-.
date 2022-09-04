1e0cf01ed049ba6273d813e6464f5922475965c6
Sagou <anastasiyazadybinina@gmailcom>
2022-09-04 11:14:10 +0300

    hw_26

--- hw_26/Program.cs
+++ hw_26/Program.cs
@@ -1,1 +1,17 @@
-﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите число начала промежутка: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите число конца промежутка: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write(GetNumbers(m,n));
+
+int GetNumbers(int start, int end)
+{
+    if(start == end) return start;
+    Console.Write(start);
+    Console.Write(",");
+    return GetNumbers(start + 1, end);
+}
